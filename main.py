fa1baa02e0d0db0d54879119a27b43bbe0352f86
scrubjay55 <karayay12@gmail.com>
2021-03-18 19:30:20 +0300

    while true

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from environ import (
 )
 from subprocess import PIPE, Popen
 import PyUploadGram
+from time import sleep
 
 
 def prep_reddit_video(video_url: str) -> bytes:
@@ -28,31 +29,33 @@ def main():
     save_bot = rBot(useragent, client_id, client_secret, bot_username, bot_pass)
     pyuploadgram_sesh = PyUploadGram.Session()
 
-    for notif in save_bot.check_inbox(rkind='t1', read_if_not_rkind=True):
-        save_bot.read_notifs([notif])
-        if 'save' not in notif.body.lower():
-            continue
-        post = save_bot.get_info_by_id(notif.post_id)
-        if post.is_video:
-            if post.domain == "v.redd.it":
-                b_video = prep_reddit_video(post.video_url)
+    while True:
+        for notif in save_bot.check_inbox(rkind='t1', read_if_not_rkind=True):
+            save_bot.read_notifs([notif])
+            if 'save' not in notif.body.lower():
+                continue
+            post = save_bot.get_info_by_id(notif.post_id)
+            if post.is_video:
+                if post.domain == "v.redd.it":
+                    b_video = prep_reddit_video(post.video_url)
+                else:
+                    # TODO
+                    continue
+                uploaded_file = pyuploadgram_sesh.upload_file(filename=post.title,
+                                                              file=b_video
+                                                              )
+                save_bot.send_reply(f"hazır [{post.title}]({uploaded_file.url})", notif)
+
+            elif post.is_img:
+                img_url = post.url
+                uploaded_file = pyuploadgram_sesh.upload_file(filename=post.title,
+                                                              file=img_url
+                                                              )
+                save_bot.send_reply(f"hazır [{post.title}]({uploaded_file.url})", notif)
             else:
                 # TODO
                 continue
-            uploaded_file = pyuploadgram_sesh.upload_file(filename=post.title,
-                                                          file=b_video
-                                                          )
-            save_bot.send_reply(f"hazır [{post.title}]({uploaded_file.url})", notif)
-
-        elif post.is_img:
-            img_url = post.url
-            uploaded_file = pyuploadgram_sesh.upload_file(filename=post.title,
-                                                          file=img_url
-                                                          )
-            save_bot.send_reply(f"hazır [{post.title}]({uploaded_file.url})", notif)
-        else:
-            # TODO
-            continue
+        sleep(7)
 
 
 if __name__ == '__main__':
